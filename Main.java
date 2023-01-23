b4774c29724089720f2b6971ecae5153cce8b42b
th4r1k <th4r1k@gmail.com>
2023-01-23 18:10:36 -0300

    feat: add terminal interaction

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
+import utils.Persist;
+import utils.Start;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        Persist.createFiles();
         System.out.println("*** Bem vindo ao ToDoApp ***");
-       
+        Start.menu();
 
     }
 }
